961d96cc162a01b6e4c871c140dbc98033d96370
Lucas Silva <69327287+lucas-Dk@users.noreply.github.com>
2020-09-27 13:26:38 -0300

    Update easyd.py

--- easyd.py
+++ easyd.py
@@ -3,11 +3,9 @@ import urllib.request
 import sys
 from datetime import datetime
 import time
-
 # Ano em que o script está sendo rodado
 ano = datetime.now().year
 print('\033[1mVocê está rodando este programa em {}\033[m'.format(ano))
-
 # Função de início
 def titulo(texto):
 	print(texto)
@@ -26,7 +24,7 @@ def baixar_arquivo(url, local):
 # Início do programa 
 print('\nVerificando seu sistema operacional...')
 time.sleep(2)
-if sys.platform == "linux2" and sys.platform == "linux":
+if sys.platform == "linux2" or sys.platform == "linux":
 	print('\033[1;32mSistema operacional compativel :)\033[m')
 	while True:
 		# Chamando a função que mostra o nome
@@ -45,25 +43,25 @@ if sys.platform == "linux2" and sys.platform == "linux":
 		print()
 		print()
 		print("""\033[1;95m
-	######################################################################
+######################################################################
 
-					   \033[1;94mMINHAS REDES\033[m
-	\033[m
-	\033[1;32mMeu facebook >\033[m https://www.facebook.com/Walker.Lxrd/
-	\033[1;32mLink do meu github > \033[mhttps://github.com/lucas-Dk
+				   \033[1;94mMINHAS REDES\033[m
+\033[m
+\033[1;32mMeu facebook >\033[m https://www.facebook.com/Walker.Lxrd/
+\033[1;32mLink do meu github > \033[mhttps://github.com/lucas-Dk
 
-	----------------------------------------------------------------------
-	Exemplo de uso: 
-	\033[1;96m
-	url = https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/FullMoon2010.jpg/280px-FullMoon2010.jpg
+----------------------------------------------------------------------
+Exemplo de uso: 
+\033[1;96m
+url = https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/FullMoon2010.jpg/280px-FullMoon2010.jpg
 
-	local = Users\\any\\downloads\\imagem.jpg  Seu arquivo será salvo neste caminho e terá esse nome\033[m
+local = Users\\any\\downloads\\imagem.jpg  Seu arquivo será salvo neste caminho e terá esse nome\033[m
 
-	----------------------------------------------------------------------
-	\033[1;31m
-	OBS: LEIA O README.md !!!\033[m\033[1;95m
+----------------------------------------------------------------------
+\033[1;31m
+OBS: LEIA O README.md !!!\033[m\033[1;95m
 
-	######################################################################
+######################################################################
 	\033[m	""")
 
 		print()
